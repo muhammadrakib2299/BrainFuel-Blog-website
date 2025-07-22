b3082680474d8c83304ea80c7e252cdae9d44dae
rakib2299 <mdrakib@combosoft.co.uk>
2025-07-22 13:42:55 +0600

    style heros button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Hero() {
   return (
     <div>
@@ -14,9 +16,27 @@ function Hero() {
               self-growth, and beyond. Whether you're here to learn, explore, or
               just take a mental break, you're in the right place.
             </p>
-            <div className="flex justify-center items-center content-center gap-2">
-              <button className="btn btn-primary">Read Blogs</button>
-              <button className="btn btn-primary">See Bookmarks</button>
+            <div className="flex justify-center items-center content-center gap-2 pt-2">
+              <Link
+                to="./blogs"
+                class="relative inline-block px-4 py-2 font-medium group"
+              >
+                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+                <span class="relative text-black group-hover:text-white">
+                  See Blogs
+                </span>
+              </Link>
+              <Link
+                to="./bookmarks"
+                class="relative inline-block px-4 py-2 font-medium group"
+              >
+                <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                <span class="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+                <span class="relative text-black group-hover:text-white">
+                  See Bookmarks
+                </span>
+              </Link>
             </div>
           </div>
         </div>
