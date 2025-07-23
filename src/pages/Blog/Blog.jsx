eedd1d16b61d8d2d18037e99c1533a3546eb7196
rakib2299 <mdrakib@combosoft.co.uk>
2025-07-23 13:40:09 +0600

    modify the tabs

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 function Blog() {
   const blog = useLoaderData();
@@ -13,6 +13,7 @@ function Blog() {
   return (
     <div className="p-5">
       <div className="max-w-4xl px-6 py-16 mx-auto space-y-12">
+        {/* Blog heading */}
         <article className="space-y-8 dark:bg-gray-100 dark:text-gray-900">
           <div className="space-y-6">
             <h1 className="text-4xl font-bold leading-15 md:tracking-tight md:text-5xl">
@@ -31,6 +32,57 @@ function Blog() {
               </p>
             </div>
           </div>
+
+          {/* Tabs bar */}
+          <div className="flex items-center -mx-4 overflow-x-auto overflow-y-hidden sm:justify-center flex-nowrap dark:bg-gray-100 dark:text-gray-800">
+            <Link className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4"
+              >
+                <path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <span>Architecto</span>
+            </Link>
+            <Link className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border border-b-0 rounded-t-lg dark:border-gray-600 dark:text-gray-900">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4"
+              >
+                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+              </svg>
+              <span>Corrupti</span>
+            </Link>
+            <Link className="flex items-center flex-shrink-0 px-5 py-3 space-x-2 border-b dark:border-gray-600 dark:text-gray-600">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-4 h-4"
+              >
+                <circle cx="12" cy="12" r="10"></circle>
+                <polygon points="16.24 7.76 14.12 14.12 7.76 16.24 9.88 9.88 16.24 7.76"></polygon>
+              </svg>
+              <span>Consectetur</span>
+            </Link>
+          </div>
         </article>
 
         <div>
@@ -48,13 +100,6 @@ function Blog() {
               className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50"
             >
               #TailwindCSS
-            </a>
-            <a
-              rel="noopener noreferrer"
-              href="#"
-              className="px-3 py-1 rounded-sm hover:underline dark:bg-violet-600 dark:text-gray-50"
-            >
-              #Angular
             </a>
           </div>
           <div className="space-y-2">
