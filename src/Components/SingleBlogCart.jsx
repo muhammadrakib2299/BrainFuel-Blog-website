fc2c8708796d1235f3943be7a9e6f9edc9906d29
rakib2299 <mdrakib@combosoft.co.uk>
2025-07-23 12:37:57 +0600

    update ui style in single blog cart

--- src/Components/SingleBlogCart.jsx
+++ src/Components/SingleBlogCart.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import placeholder from "../assets/404.jpg";
 
 function SingleBlogCart({ blog }) {
   const { cover_image, title, description, published_at } = blog;
 
   return (
-    <div>
+    <div className="bg-gray-900 p-3 rounded-lg">
       <Link
         to={`/blog/$(id)`}
-        className="max-w-sm mx-auto group hover:no-underline focus:no-underline bg-gray-900"
+        className="max-w-sm mx-auto group hover:no-underline focus:no-underline"
       >
         <img
           role="presentation"
           className="object-cover w-full rounded h-44 bg-gray-500"
-          src={cover_image}
+          src={cover_image || placeholder}
         />
+
         <div className="p-6 space-y-2">
-          <h3 className="text-black text-2xl font-semibold group-hover:underline group-focus:underline">
+          <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
             {title}
           </h3>
-          <span className="text-xs text-gray-400">{published_at}</span>
-          <p className="text-black py-4">{description}</p>
+          <span className="text-xs text-white">{published_at}</span>
+          <p className="text-white py-4">{description}</p>
         </div>
       </Link>
     </div>
--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -12,12 +12,12 @@ function Blogs() {
           {/* Show the 1st blog */}
           <Link
             to={`blogs/${blogs[0].id}`}
-            className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900 bg-gray-900"
+            className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-gray-900"
           >
             <img
               src={blogs[0].cover_image}
               alt=""
-              className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 bg-gray-500"
+              className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 "
             />
             <div className="p-6 space-y-2 lg:col-span-5 py-8">
               <h3 className="text-2xl font-semibold sm:text-4xl group-hover:underline group-focus:underline">
