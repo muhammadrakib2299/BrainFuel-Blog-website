857732b32e2710e1eec41a4aab85c64a3e25819f
rakib2299 <mdrakib@combosoft.co.uk>
2025-08-06 11:42:55 +0600

    added delete button in bookmark

--- src/Components/SingleBlogCart.jsx
+++ src/Components/SingleBlogCart.jsx
@@ -1,33 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import placeholder from "../assets/404.jpg";
+import { MdDeleteForever } from "react-icons/md";
 
-function SingleBlogCart({ blog }) {
+function SingleBlogCart({ blog, deletable }) {
   const { cover_image, title, description, published_at, id } = blog;
 
   return (
-    <div className="bg-gray-900 p-3 rounded-lg">
-      <Link
-        to={`/blog/${id}`}
-        className="max-w-sm mx-auto group hover:no-underline focus:no-underline"
-      >
-        <img
-          role="presentation"
-          className="object-cover w-full rounded h-44 bg-gray-500"
-          src={cover_image || placeholder}
-        />
+    <div className="flex relative">
+      <div className="bg-gray-900 p-3 rounded-lg">
+        <Link
+          to={`/blog/${id}`}
+          className="max-w-sm mx-auto group hover:no-underline focus:no-underline"
+        >
+          <img
+            role="presentation"
+            className="object-cover w-full rounded h-44 bg-gray-500"
+            src={cover_image || placeholder}
+          />
 
-        <div className="p-6 space-y-2">
-          <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
-            {title}
-          </h3>
-          <span className="text-xs text-white">
-            {" "}
-            {new Date(published_at).toLocaleDateString()}
-          </span>
-          <p className="text-white py-4">{description}</p>
-        </div>
-      </Link>
+          <div className="p-6 space-y-2">
+            <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+              {title}
+            </h3>
+            <span className="text-xs text-white">
+              {" "}
+              {new Date(published_at).toLocaleDateString()}
+            </span>
+            <p className="text-white py-4">{description}</p>
+          </div>
+        </Link>
+        {deletable && (
+          <div className="absolute bg-primary p-3 rounded-full -top-5 -right-5 cursor-pointer hover:scale-105">
+            <MdDeleteForever
+              size={23}
+              className="text-white group-hover:text-primary"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/pages/Bookmarks/Bookmarks.jsx
+++ src/pages/Bookmarks/Bookmarks.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { getBlogs } from "../../utils";
 import SingleBlogCart from "../../Components/SingleBlogCart";
+import { Link } from "react-router-dom";
 
 function Bookmarks() {
   const [blogs, setBlogs] = useState([]);
@@ -12,11 +13,33 @@ function Bookmarks() {
 
   return (
     <div className="container py-15 px-6 max-w-7xl mx-auto space-y-6 sm:space-y-12 text-white">
-      <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-        {blogs.map((blog) => (
-          <SingleBlogCart key={blog.id} blog={blog}></SingleBlogCart>
-        ))}
-      </div>
+      {blogs.length === 0 ? (
+        <div className="text-center">
+          <h1 className="text-3xl text-gray-700 font-bold text-center mt-20 pb-5">
+            No Bookmarks Found
+          </h1>
+          <Link
+            to="/blogs"
+            class="relative inline-block px-4 py-2 font-medium group"
+          >
+            <span class="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-primary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+            <span class="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-primary"></span>
+            <span class="relative text-black group-hover:text-white">
+              Browse Blogs
+            </span>
+          </Link>
+        </div>
+      ) : (
+        <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          {blogs.map((blog) => (
+            <SingleBlogCart
+              deletable={true}
+              key={blog.id}
+              blog={blog}
+            ></SingleBlogCart>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
