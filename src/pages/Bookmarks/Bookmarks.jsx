f3cfa5a5c1b1e3f28e5cfb9f359dfac6b0a1d6af
rakib2299 <mdrakib@combosoft.co.uk>
2025-08-06 11:10:51 +0600

    succesfully added blog in bookmarks

--- src/pages/Bookmarks/Bookmarks.jsx
+++ src/pages/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getBlogs } from "../../utils";
+import SingleBlogCart from "../../Components/SingleBlogCart";
 
 function Bookmarks() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const storedBlogs = getBlogs();
+    setBlogs(storedBlogs);
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-7xl">Welcome Bookmarks</h1>
+    <div className="container py-15 px-6 max-w-7xl mx-auto space-y-6 sm:space-y-12 text-white">
+      <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {blogs.map((blog) => (
+          <SingleBlogCart key={blog.id} blog={blog}></SingleBlogCart>
+        ))}
+      </div>
     </div>
   );
 }
